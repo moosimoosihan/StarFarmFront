fffcb76701e07b7718b39f8f7ff1660dc4764d7e
alicia0419 <alicia20010419@gmail.com>
2023-12-18 15:47:01 +0900

    결제 테스트

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="hello">
+    <!-- <button @click="paytest()">결제버튼</button> -->
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -31,11 +32,46 @@
 </template>
 
 <script>
+// import { Bootpay } from '@bootpay/client-js'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   }
+  // methods: {
+  //       async paytest() {
+  //           const response = await Bootpay.requestPayment({
+  //           "application_id": "657fe1f7e57a7e001b5a00b2",
+  //           "price": 1000,
+  //           "order_name": "테스트결제",
+  //           "order_id": "TEST_ORDER_ID",
+  //           "pg": "다날",
+  //           "method": "카드",
+  //           "tax_free": 0,
+  //           "user": {
+  //               "id": "회원아이디",
+  //               "username": "회원이름",
+  //               "phone": "01000000000",
+  //               "email": "test@test.com"
+  //           },
+  //           "items": [
+  //               {
+  //               "id": "item_id",
+  //               "name": "테스트아이템",
+  //               "qty": 1,
+  //               "price": 1000
+  //               }
+  //           ],
+  //           "extra": {
+  //               "open_type": "iframe",
+  //               "card_quota": "0,2,3",
+  //               "escrow": false
+  //           }
+  //           })
+
+  //           console.log(response)
+  //       }
+  //   }
 }
 </script>
 
