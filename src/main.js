e4ff7720d53a47529b53fd18c302da6cccbc718e
segun <segun408@gmail.com>
2023-12-19 18:42:55 +0900

    vue3 로 변경 후 라우터 적용

--- src/main.js
+++ src/main.js
@@ -1,8 +1,41 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-Vue.config.productionTip = false
+// 레이아웃
+import defaultLayout from './layouts/defaultLayout.vue'
+// import adminLayout from './layouts/adminLayout.vue'
+// import mypageLayout from './layouts/mypageLayout.vue'
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// 메인 페이지
+
+// 마이 페이지
+
+// 관리자 페이지
+
+import MainPage from './views/MainPage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'defaultLayout',
+    component: defaultLayout,
+    children: [
+      {
+        path: '/',
+        name: 'MainPage',
+        component: MainPage
+      }
+    ]
+}]
+
+// window.Kakao.init('카카오 앱 키');
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
