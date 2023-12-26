074cea19c96c5f8b3113c284400f853b977aa81e
Seunghoim <tngkrwhdgkq@naver.com>
2023-12-26 15:17:24 +0900

    payment수정AdminUser수정정  및 주문상세보기페이지작성완료

--- src/views/payment.vue
+++ src/views/payment.vue
@@ -1,43 +1,139 @@
 <template>
-    <div class="container">
-        <div id="payment_wrapper">
-            <form id="payment_form">
-                <h1>상품 결제</h1>
-                <div id="payment_product">
-                    상품 정보
-                    리뷰 페이지에서 가져오면 됨
-                </div>
-                <div id="payment_user_name">
-                    <p>수령자 이름 *</p>
-                    <input type="text" placeholder="이름">
-                </div>
-                <div id="payment_phone">
-                    <p>전화번호 *</p>
-                    <input type="text" placeholder="전화번호">
-                </div>
-                <div id="payment_deliv">
-                    <p>배송지 *</p>
-                    <input type="text" placeholder="주소" id="payment_address1">
-                    <input type="button" value="우편번호" id="payment_address_btn">
-                    <input type="text" placeholder="상세 주소" id="payment_address2">
-                </div>
-                <div id="payment_request">
-                    <p>배송 시 요청사항</p>
-                    <textarea placeholder="배송 요청사항을 입력해주세요"></textarea>
-                </div>
-                <div id="payment_submit">
-                    <div id="payment_pay_btn">
-                        <a href="" style="text-decoration:none;color:black">취소</a>
-                    </div>
-                    <input type="submit" value="결제" id="payment_pay_btn">
-                </div>
-            </form>
+  <div class="container">
+    <div id="payment_wrapper">
+      <form id="payment_form">
+        <h1>상품 결제</h1>
+        <div id="payment_product">
+          <!-- 상품 정보 가져오면 됨 -->
         </div>
-    </div>
-</template>
-<script>
+        <div id="payment_user_name">
+          <p>수령자 이름 *</p>
+          <input type="text" placeholder="이름">
+        </div>
+        <div id="payment_phone">
+          <p>전화번호 *</p>
+          <input type="text" placeholder="전화번호">
+        </div>
+        <div id="payment_deliv">
+          <p>배송지 *</p>
+          <input type="text" id="sample6_postcode" placeholder="우편번호">
+          <button @click="openKakaoMap">우편번호 찾기</button><br>
+          <input type="text" id="sample6_address" placeholder="주소"><br>
+          <input type="text" id="sample6_detailAddress" placeholder="상세주소">
+          <input type="text" id="sample6_extraAddress" placeholder="참고항목">
+         </div>
+         <div id="payment_request">
+           <p>배송 시 요청사항</p>
+           <textarea placeholder="배송 요청사항을 입력해주세요"></textarea>
+         </div>
+         <div id="payment_submit">
+         <div id="payment_pay_btn">
+           <a href="./search_goodslist" style="text-decoration:none;color:black">취소</a>
+         </div>
+            <input type="submit" value="결제" id="payment_pay_btn" @click="processPayment">
+         <div v-if="showSendDataButton">
+        </div>
+        </div>
+        <div v-for="(item, index) in reports" :key="index">
+         {{ report.name }} 
+         {{ report.Num }}
+         {{ report.Address }}
+         {{ report.AddressHope }}
+         {{ report.price}}
+        </div>
+        </form>
+        </div>
+        </div>
+        </template>
+        <script>
+        export default {
+        data() {
+        return {
+          showSendDataButton: false, // 조건에 따라 버튼을 표시할지 여부를 결정하는 변수
+        items: [
+        // 실제 데이터를 추가하거나, 사용자 입력 시 동적으로 할당
+        {
+          name: '상품1',
+          Num: '01012345678', // 전화번호 형식으로 변경
+          Address: '주소1',
+          AddressHope: '참고항목1',
+          price:'20000'
+         },
+         {
+          name: '상품2',
+          Num: '01098765432', // 다른 전화번호 예시
+          Address: '주소2',
+          AddressHope: '참고항목2',
+          price:'20000'
+        },
+        ],
+        postcodeResult: null, // 우편번호 검색 결과를 저장할 변수 추가
+        };
+        },
+        mounted() {
+     
+        const script = document.createElement("script");
+        script.src = "//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
+        script.onload = this.initDaumPostcode;
+        document.head.appendChild(script);
+        },
 
+
+        methods: {
+                 //주문상세보기 페이지 연결
+        processPayment() {
+          this.$router.push({path:'/paymentDetail'})
+        },
+        
+        sendDataToBackend() {
+           initDaumPostcode() 
+        // 서버로 데이터를 전송하는 로직
+        const dataToSend = {
+        // 원하는 데이터를 이 부분에 추가
+        // 예시로 수령자 이름, 전화번호 등을 추가했으나 실제로 필요한 데이터로 교체해야 함
+        recipientName: this.recipientName,
+        phoneNumber: this.phoneNumber,
+        // 이하 필요한 데이터 추가
+        };  
+
+        // Axios를 사용하여 서버로 데이터 전송
+        axios.post('localhost:8081/paymentDetail', dataToSend)
+        .then(response => {
+          // 전송 성공 시 실행할 로직
+          console.log('데이터 전송 성공');
+         })
+        .catch(error => {
+          // 전송 실패 시 실행할 로직
+          console.error('데이터 전송 실패', error);
+         });
+         },
+        openKakaoMap() {
+     
+        new daum.Postcode({
+        oncomplete: (data) => {
+        
+
+          this.postcodeResult = data;
+          this.handlePostcodeResult(data);
+         }
+         }).open();
+         },
+
+         handlePostcodeResult(data) {
+    
+        this.$nextTick(() => {
+        
+        document.getElementById('sample6_postcode').value = data.zonecode;
+        document.getElementById('sample6_address').value = data.address;
+        document.getElementById('sample6_detailAddress').value = ''; // 상세주소 초기화
+        document.getElementById('sample6_extraAddress').value = ''; // 참고항목 초기화
+         this.$router.push('/paymentDetail');
+      });
+      },
+      }
+      };
 </script>
+
 <style scoped>
 * {
     margin: 0;
@@ -115,7 +211,7 @@
     font-size: 15px;
 
 }
-#payment_address_btn {
+.payment_address_btn {
     width: 80px;
     height: 30px;
 }
@@ -157,5 +253,6 @@
     margin-left: 30px;
     text-align: center;
     font-size:17px
+    
 }
 </style>
---
+++ src/views/paymentDetail.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="container">
+    <div id="payment_wrapper">
+      <form id="payment_form">
+        <h1>주문 상세 보기</h1>
+
+        
+        <div class="info-row">
+          <p class="info-label">수령자 이름 *</p>
+          <p class="info-value">{{ userInfo.name }}</p>
+        </div>
+
+        <!-- 전화번호 -->
+        <div class="info-row">
+          <p class="info-label">전화번호 *</p>
+          <p class="info-value">{{ userInfo.phone }}</p>
+        </div>
+
+        <!-- 배송지 주소 -->
+        <div class="info-row">
+          <p class="info-label">배송지 *</p>
+          <p class="info-value">{{ userInfo.address }}</p>
+        </div>
+
+        <!-- 상세주소, 참고항목 -->
+        <div class="info-row">
+          <p class="info-label">상세주소 *</p>
+          <p class="info-value">{{ userInfo.detailAddress }}</p>
+        </div>
+
+        <!-- 배송 요청사항 -->
+        <div class="info-row">
+          <p class="info-label">배송 요청사항</p>
+          <p class="info-value">{{ userInfo.deliveryRequest }}</p>
+        </div>
+
+        <!-- 상품 정보 -->
+        <div id="payment_product">
+          <p>{{ productInfo.name }}</p>
+          <p>{{ productInfo.number }}</p>
+          <p>{{ productInfo.address }}</p>
+          <p>{{ productInfo.addressHope }}</p>
+          <!-- 상품 상세 정보 표시 -->
+        </div>
+
+        <div id="payment_submit">
+          <div id="home_button">
+            <router-link to="./" style="text-decoration: none; color: white;">
+              홈으로
+            </router-link>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userInfo: {
+        name: '홍길동',
+        phone: '010-1234-5678',
+        address: '서울시 강남구',
+        detailAddress: '123동 456호',
+        deliveryRequest: '부재 시 문 앞에 놓아주세요.',
+      },
+      productInfo: {
+        name: '상품1',
+        number: '12345',
+        address: '상품 배송지',
+        addressHope: '참고항목',
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+
+#payment_wrapper {
+  width: 70%;
+}
+
+#payment_form h1 {
+  margin-bottom: 16%;
+}
+
+.info-row {
+  display: flex;
+  margin-bottom: 80px;
+}
+
+.info-label {
+  font-size: 18px;
+  font-weight: 700;
+  margin-right: 10px;
+  margin-bottom: 20px;
+}
+
+.info-value {
+  font-size: 18px;
+}
+#payment_submit {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 10%;
+}
+
+#home_button {
+  width: 100px;
+  height: 50px;
+  background-color: pink;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+</style>
