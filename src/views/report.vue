3bf3c35f7e4a61e3c42f578ce3c87c3ba57e3918
segun <segun408@gmail.com>
2024-01-04 18:48:19 +0900

    신고하기 구현

--- src/views/report.vue
+++ src/views/report.vue
@@ -5,26 +5,25 @@
     <div class="report_user">
     <div class="report_user_box">
         <p>신고 할 유저 :</p>
-        <input type="text" placeholder="닉네임(이메일)" class="report_nick">
+        <input type="text" class="report_nick" v-model="reportUser.user_nick" disabled>
     </div>
     <div class="report_title_box">
-        <select class="report_category">
-        <option>상품신고</option>
-        <option>판매자 신고</option>
-        <option>구매자 신고</option>
+        <select class="report_category" v-model="reportCategory">
+        <option value="상품신고" selected>상품신고</option>
+        <option value="판매자신고">판매자 신고</option>
+        <option value="구매자신고">구매자 신고</option>
         </select>
-        <textarea placeholder="글 제목" class="report_title"></textarea>
+        <textarea placeholder="글 제목" class="report_title" v-model="reportTitle"></textarea>
     </div>
     <div class="report_file_box">
-                    <input type="button" value="파일 업로드" class="report_upload_file">
+                    <input type="file" class="report_upload_file" @change="uploadFile($event.target.files)" >
+                    <img v-if="reportFileSrc!=''" :src="reportFileSrc" alt="미리보기" />
     <div class="report_file_name">파일 제목</div>
     </div>
     </div>
-        <form class="report_submit">
-        <textarea class="report_content"></textarea>
-        <input type="submit" value="신고하기" class="report_submit_btn">
-        <input type="button" value="취소" class="report_submit_btn">
-        </form>
+        <textarea class="report_content" v-model="reportContent"></textarea>
+        <button class="report_submit_btn" @click="reportSubmit()">신고하기</button>
+        <button class="report_submit_btn" @click="gotoBack()">취소</button>
     </div>
     </div>
 </template>
@@ -33,13 +32,125 @@ import axios from 'axios'
 export default {
     data() {
         return {
-            report: {
-                user: '',
-                category: '',
-                title: '',
-                content: '',
-                file: ''
+            loginUser: {},
+            reportUser: {},
+            reportUserNo: '',
+            reportCategory: '상품신고',
+            reportTitle: '',
+            reportContent: '',
+            reportFile: '',
+            reportFileSrc: '',
+        }
+    },
+    created() {
+        this.getReportUser()
+        this.getUser()
+    },
+    computed: {
+        user() {
+            return this.$store.state.user;
+        },
+    },
+    methods: {
+        async getReportUser() {
+            try{
+                const goods_no = this.$route.params.id
+                const response = await axios.get(`http://localhost:3000/goods/getUserNo/${goods_no}`)
+                this.reportUserNo = response.data[0].user_no
+                try{
+                    const response1 = await axios.get(`http://localhost:3000/mypage/mypage/${this.reportUserNo}`)
+                    this.reportUser = response1.data[0]
+                } catch (error) {
+                    console.log(error)
+                }
+            } catch (error) {
+                console.log(error)
             }
+        },
+        async getUser() {
+            const user_no = this.user.user_no
+            try{
+                const response = await axios.get(`http://localhost:3000/mypage/mypage/${user_no}`)
+                this.loginUser = response.data[0]
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        async reportSubmit() {
+            if(this.reportTitle == ''){
+                this.$swal("제목을 입력해주세요")
+                return
+            }
+            if(this.reportContent == ''){
+                this.$swal("내용을 입력해주세요")
+                return
+            }
+
+            try{
+                await axios.post(`http://localhost:3000/auth/report`, {
+                    report_title: this.reportTitle,
+                    report_category: this.reportCategory,
+                    report_content: this.reportContent,
+                    report_user_no: this.reportUserNo,
+                    user_no: this.loginUser.user_no,
+                    report_img: this.reportFile
+                })
+                .then((res) => {
+                    if (res.data.message == 'success'){
+                        this.$swal("신고 완료")
+                        this.$router.push(`/product/${this.$route.params.id}`)
+                    }
+                    else {
+                        this.$swal("DB 에러");
+                    }
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        async uploadFile(file) {
+            let name = "";
+            if (file) {
+                name = file[0].name;
+            }
+            else {
+                return;     // 파일 미선택 시 반환
+            }
+
+            const formData = new FormData();
+            
+            formData.append('img', file[0]);
+
+            this.reportFileSrc = URL.createObjectURL(file[0]);
+
+            for (let key of formData.keys()) {
+                console.log(key, ":", formData.get(key));
+            }
+            try {
+                axios({
+                    url: `http://localhost:3000/auth/upload_img`,
+                    method: 'POST',
+                    headers: {'Content-Type': 'multipart/form-data'},
+                    data: formData
+                })
+                    .then ((res) => {
+                        if (res.data.message != 'success'){
+                            this.$swal("DB 에러");
+                        }
+                    })
+                    .catch(e => {
+                        console.log(e);
+                    })
+                this.reportFile = name;
+                console.log(this.reportFile)
+                return true;
+            } catch(err){
+                console.log(err);
+                return false;
+            }
+        },
+        gotoBack() {
+            this.$router.push(`/product/${this.$route.params.id}`)
         }
     }
 }
